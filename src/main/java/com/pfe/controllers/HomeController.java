8e9d5e56a9ca0280058ef2ca673473beece6c5c1
ShinyFox <ahmedrida21@gmail.com>
2020-11-30 00:39:57 +0100

    project setup

---
+++ src/main/java/com/pfe/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.pfe.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping
+	private String HomeMessage() {
+		
+		return "Welcome to the website!";
+	}
+}
