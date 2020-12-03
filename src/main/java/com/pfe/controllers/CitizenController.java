8105a416433778141369eda7164724480bb4b235
Alfa Khrisna <alfa.khrisna@student.vinci.be>
2020-12-03 21:13:41 +0100

    IMPL: Changed register to a post method.

--- src/main/java/com/pfe/controllers/CitizenController.java
+++ src/main/java/com/pfe/controllers/CitizenController.java
@@ -2,7 +2,7 @@ package com.pfe.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pfe.models.Citizen;
@@ -14,7 +14,7 @@ public class CitizenController {
 	@Autowired
 	private ICitizenService citizenservice;
 
-	@GetMapping("/citizen/register")
+	@PostMapping("/citizen/register")
 	private Citizen createCitizen() {
 		return citizenservice.createCitizen();
 	}
