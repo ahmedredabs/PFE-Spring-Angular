fa5f2595cd232891baabdf43c3a39fe206a6264f
Alfa Khrisna <alfa.khrisna@student.vinci.be>
2020-12-08 15:33:19 +0100

    IMPL: Token utility class (Also cleaned unused encoder)

---
+++ src/main/java/com/pfe/utils/TokenManager.java
@@ -0,0 +1,27 @@
+package com.pfe.utils;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class TokenManager {
+	private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+	public static final String TOKEN_PREFIX = "Bearer ";
+	private static final int TOKEN_EXPIRY_DURATION = 10; // in days
+
+	public static String generateToken(String subject) {
+		return Jwts.builder().setSubject(subject)
+				.setExpiration(Date.from(ZonedDateTime.now().plusDays(TOKEN_EXPIRY_DURATION).toInstant()))
+				.signWith(SECRET_KEY).compact();
+	}
+
+	public static String parseToken(String token) {
+		return Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+				.getBody().getSubject();
+	}
+}
