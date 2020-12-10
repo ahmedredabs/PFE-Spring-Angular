2c21865ef69d24df72ff4f570b8edb4fe5da6d8d
Alfa Khrisna <alfa.khrisna@student.vinci.be>
2020-12-10 23:25:39 +0100

    FIX: sql aeugh

--- src/main/java/com/pfe/repositories/ScanRepository.java
+++ src/main/java/com/pfe/repositories/ScanRepository.java
@@ -1,16 +1,14 @@
 package com.pfe.repositories;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import com.pfe.models.Physician;
 import com.pfe.models.Scan;
 
-public interface ScanRepository extends CrudRepository<Scan,Long> {
-	
-	@Query(value = "SELECT * FROM SCANS WHERE QRCODE_id=?1 AND CURRENT_DATE <= SCAN_DATE + interval '10' day",nativeQuery=true)
+public interface ScanRepository extends CrudRepository<Scan, Long> {
+
+	@Query(value = "SELECT * FROM SCANS WHERE qr_code_id=?1 AND CURRENT_DATE <= SCAN_DATE + interval '10' day", nativeQuery = true)
 	List<Scan> findScansByCitizenId(long contaminedCitizenId);
 }
