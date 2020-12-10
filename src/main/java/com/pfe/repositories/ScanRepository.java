c65330c3b7a6a70790417ce109904256d8941f42
Alfa Khrisna <alfa.khrisna@student.vinci.be>
2020-12-10 23:44:11 +0100

    FIX: speedrun time bois

--- src/main/java/com/pfe/repositories/ScanRepository.java
+++ src/main/java/com/pfe/repositories/ScanRepository.java
@@ -9,6 +9,9 @@ import com.pfe.models.Scan;
 
 public interface ScanRepository extends CrudRepository<Scan, Long> {
 
-	@Query(value = "SELECT * FROM SCANS WHERE qr_code_id=?1 AND CURRENT_DATE <= SCAN_DATE + interval '10' day", nativeQuery = true)
+	@Query(value = "SELECT * FROM SCANS WHERE CITIZEN_ID=?1 AND CURRENT_DATE <= SCAN_DATE + interval '10' day", nativeQuery = true)
 	List<Scan> findScansByCitizenId(long contaminedCitizenId);
+
+	@Query(value = "SELECT * FROM SCANS WHERE QR_CODE_id=?1 AND CURRENT_DATE <= SCAN_DATE + interval '10' day", nativeQuery = true)
+	List<Scan> findScansByQRId(long qrId);
 }
--- src/main/java/com/pfe/services/ScanService.java
+++ src/main/java/com/pfe/services/ScanService.java
@@ -13,26 +13,29 @@ import com.pfe.repositories.ScanRepository;
 @Service
 public class ScanService implements IScanService {
 
-    @Autowired
-    private ScanRepository scanRepository;
-    
-    @Autowired
-    private CitizenRepository citizenRepository;
+	@Autowired
+	private ScanRepository scanRepository;
 
-    @Override
-    public Scan createVisitScan(Scan scan) {
-        return scanRepository.save(scan);
-    }
-    
-    @Override
-    public Scan createAlertScan(Scan scan) {
-    	Scan scanInsere = scanRepository.save(scan);
-    	List<Scan> listOfUsersToAlert= scanRepository.findScansByCitizenId(scan.getQrCode().getId());
-    	for(Scan scanOfUserToAlert:listOfUsersToAlert) {
-    		Citizen citizenToAlert = citizenRepository.findById(scanOfUserToAlert.getCitizen().getId()).get();
-    		citizenToAlert.setAlerted(true);
-    		citizenRepository.save(citizenToAlert);
-    	}
-        return scanInsere;
-    }
+	@Autowired
+	private CitizenRepository citizenRepository;
+
+	@Override
+	public Scan createVisitScan(Scan scan) {
+		return scanRepository.save(scan);
+	}
+
+	@Override
+	public Scan createAlertScan(Scan scan) {
+		Scan scanInsere = scanRepository.save(scan);
+		List<Scan> listOfLocationsVisited = scanRepository.findScansByCitizenId(scan.getCitizen().getId());
+		for (Scan locationVisited : listOfLocationsVisited) {
+			List<Scan> locationsVisited = scanRepository.findScansByQRId(locationVisited.getQrCode().getId());
+			for (Scan scan1 : locationsVisited) {
+				Citizen citizenToAlert = citizenRepository.findById(scan1.getCitizen().getId()).get();
+				citizenToAlert.setAlerted(true);
+				citizenRepository.save(citizenToAlert);
+			}
+		}
+		return scanInsere;
+	}
 }
